57b0384741f62af63be11ce4d450bbb0b88d6065
Volodymyr <83507366+volom@users.noreply.github.com>
2022-04-03 11:53:27 +0300

    frames depends on duration

--- video2images.py
+++ video2images.py
@@ -5,6 +5,13 @@ import os
 frameRate = 0.3 # it will capture image in each 0.5 second
 
 req_video = '' #set path to video
+data = cv2.VideoCapture(req_video)
+# count the number of frames
+frames = data.get(cv2.CAP_PROP_FRAME_COUNT)
+fps = int(data.get(cv2.CAP_PROP_FPS))
+
+# calculate dusration of the video
+seconds = int(frames / fps)
 video_name = os.path.splitext(os.path.basename(req_video))[0]
 
 try:
@@ -26,7 +33,7 @@ sec = 0
 count=1
 success = getFrame(sec)
 
-while success:
+for i in range(int(round(seconds/frameRate, 0))):
     count = count + 1
     sec = sec + frameRate
     sec = round(sec, 2)
