dfbc0d7db35c3c76934a8acccee7325763d343a4
Volodymyr <83507366+volom@users.noreply.github.com>
2022-04-05 20:59:26 +0300

    Add files via upload

---
+++ remove_duplicated_images.py
@@ -0,0 +1,23 @@
+# removing duplicated images in the given directory
+
+import hashlib
+import os
+from tqdm import tqdm
+images_dir = r''
+file_list = os.listdir(images_dir)
+
+duplicates = []
+hash_keys = dict()
+
+for index, filename in  tqdm(enumerate(os.listdir(images_dir)), position=0, leave=True):  
+    if os.path.isfile(f'{images_dir}/{filename}'):
+        with open(f'{images_dir}/{filename}', 'rb') as f:
+            filehash = hashlib.md5(f.read()).hexdigest()
+        if filehash not in hash_keys: 
+            hash_keys[filehash] = index
+        else:
+            duplicates.append((index,hash_keys[filehash]))
+
+
+for index in tqdm(duplicates, position=0, leave=True):
+    os.remove(file_list[index[0]])
